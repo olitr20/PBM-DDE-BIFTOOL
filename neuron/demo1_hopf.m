04a680c91f1b91dea954c7954d929c725b577ade
olitr20 <123574069+olitr20@users.noreply.github.com>
2024-12-16 14:12:24 +0000

    Update demo1_hopf.m

--- neuron/demo1_hopf.m
+++ neuron/demo1_hopf.m
@@ -48,7 +48,7 @@ p_splot(hopf);                     % plot stability of hopf point
 % an intermediate order reversal and a second call to |br_contn|.
 branch2=df_brnch(funcs,[ind_theta_u,ind_taus],'hopf'); % use hopf point as first point of hopf branch:
 branch2.parameter.min_bound(1,:)=[ind_theta_u 0.4];
-branch2.parameter.max_bound(1:2,:)=[[ind_theta_u 1]' [ind_taus 0.2]']';
+branch2.parameter.max_bound(1:2,:)=[[ind_theta_u 1]' [ind_taus 0.5]']';
 branch2.parameter.max_step(1:2,:)=[[ind_theta_u 0.01]' [ind_taus 0.01]']';
 branch2.point=hopf;
 
@@ -56,18 +56,27 @@ hopf.parameter(ind_theta_u)=hopf.parameter(ind_theta_u)+0.01; % perturb hopf poi
 [hopf,success]=p_correc(funcs,hopf,ind_taus,[],method.point); % correct hopf point, recompute stability
 branch2.point(2)=hopf;                                 % use as second point of hopf branch:
 figure(6); clf;
-[branch2,s,f,r]=br_contn(funcs,branch2,50);            % continue with plotting hopf branch:
+[branch2,s,f,r]=br_contn(funcs,branch2,1000);            % continue with plotting hopf branch:
 branch2=br_rvers(branch2);                             % reverse Hopf branch
 [branch2,s,f,r]=br_contn(funcs,branch2,1000);            % continue in other direction
 xlabel('theta_u');ylabel('tau');
 
-%% Continue Hopf bifurcation after turning point
+%% Initializa and continue lower Hopf bifurcation branch
 branch3=df_brnch(funcs,[ind_theta_u,ind_taus],'hopf'); % use hopf point as first point of hopf branch:
 branch3.parameter.min_bound(1,:)=[ind_theta_u 0.4];
 branch3.parameter.max_bound(1:2,:)=[[ind_theta_u 1]' [ind_taus 0.2]']';
 branch3.parameter.max_step(1:2,:)=[[ind_theta_u 0.01]' [ind_taus 0.01]']';
-branch3.point=branch2.point(end);
 
+hopf2 = hopf;
+hopf2.parameter(7)=0.7; hopf2.parameter(9)=0.07;
+[hopf2,success]=p_correc(funcs,hopf2,ind_taus,[],method.point);
+branch3.point=hopf2;
+hopf2
+success
+
+% Unable to find lower hopf point %
+
+%%
 hopf.parameter(ind_taus)=hopf.parameter(ind_taus)-0.01; % perturb hopf point
 [hopf,success]=p_correc(funcs,hopf,ind_theta_u,[],method.point); % correct hopf point, recompute stability
 branch3.point(2)=branch2.point(end-1);                                 % use as second point of hopf branch:
